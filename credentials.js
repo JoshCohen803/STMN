cb15a98385b41a963be4a2d84e7ddfe39f2eddad
Josh Cohen <88695407+JoshCohen803@users.noreply.github.com>
2025-10-14 23:55:39 -0400

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -3,5 +3,6 @@
 // Edit here if you want initial accounts — after first run use Admin panel
 window.CREDENTIALS = [
   { username: 'admin', password: 'admin123', name: 'Site Admin', groups: ['Admin'] },
-  { username: 'jcohen',  password: 'Grouchomoose1234$$',  name: 'Josh',       groups: ['User'] }
+  { username: 'jcohen', password: 'Grouchomoose1234$$', name: 'Site Adminj', groups: ['Admin'] },
+  { username: 'test',  password: 'Grouchomoose1234$$',  name: 'Josh',       groups: ['User'] }
 ];
